76ae2e394f14c0506e76b72ef16506d70b0d418c
Irina Pashkovsky <111738428+IrinaPash@users.noreply.github.com>
2022-09-14 00:28:24 +0200

    Add files via upload

--- Homework/September/Homework_120922.java
+++ Homework/September/Homework_120922.java
@@ -3,16 +3,21 @@ package org.example;
 public class Homework_120922 {
 
 
-    public static void main(String[] args) {
-        /* Написать метод String replace(String str, char oldChar, char newChar) который в переданной строке str
-        меняет все символы  oldChar на символы newChar.  Например: replace(«саша», ‘c’, ‘м’) -> «маша»
-         */
+    /* Написать метод String replace(String str, char oldChar, char newChar) который в переданной строке str
+    меняет все символы  oldChar на символы newChar.  Например: replace(«саша», ‘c’, ‘м’) -> «маша»
+     */
+    private static void StringReplaceWithChar(String str, char oldChar, char newChar) {
+        String oldString = "Sascha";
+        String newString = oldString.replace('S', 'M');
+        System.out.println("oldString before replace : " + oldString + "\n");
+        System.out.println("New String : " + newString + "\n");
 
-        String oldString = "Sasha";
-        String newString =  oldString.replace('S', 'M');
-
-        System.out.println("Old string: " + oldString);
-        System.out.println("New string: " + newString);
     }
 
+    public static void main(String[] args) {
+        StringReplaceWithChar(new String("Sascha"),'S','M');
+
+    }
 }
+
+
